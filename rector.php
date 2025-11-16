4cdbf02cb19ccc52b805b4a7244562d8be2f25ba
Ruslan Kutorgin <teenxsky@gmail.com>
2025-11-16 15:50:19 +1000

    Add rector config

---
+++ rector.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
+use Rector\CodeQuality\Rector\ClassMethod\InlineArrayReturnAssignRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodeQuality\Rector\Ternary\SwitchNegatedTernaryRector;
+use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector;
+use Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
+use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
+use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
+use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
+use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnDirectArrayRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNewArrayRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedCallRector;
+use Rector\TypeDeclaration\Rector\Closure\ClosureReturnTypeRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use Rector\ValueObject\PhpVersion;
+use RectorLaravel\Rector\MethodCall\ResponseHelperCallToJsonResponseRector;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+    ]);
+
+    $rectorConfig->skip([
+        RenameParamToMatchTypeRector::class,
+        RenamePropertyToMatchTypeRector::class
+    ]);
+
+    $rectorConfig->rules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        DeclareStrictTypesRector::class,
+        ClosureReturnTypeRector::class,
+        ReturnTypeFromStrictTypedCallRector::class,
+        ReturnTypeFromStrictNewArrayRector::class,
+        ReturnTypeFromReturnDirectArrayRector::class,
+        TypedPropertyFromStrictConstructorRector::class,
+        CompleteDynamicPropertiesRector::class,
+        InlineArrayReturnAssignRector::class,
+        ExplicitBoolCompareRector::class,
+        SwitchNegatedTernaryRector::class,
+        NewlineBeforeNewAssignSetRector::class,
+        EncapsedStringsToSprintfRector::class,
+        PostIncDecToPreIncDecRector::class,
+        SymplifyQuoteEscapeRector::class,
+        RemoveUnusedPromotedPropertyRector::class,
+        RemoveUnusedPrivateMethodRector::class,
+        RemoveUnusedPrivatePropertyRector::class,
+        ClassPropertyAssignToConstructorPromotionRector::class,
+        ReadOnlyPropertyRector::class,
+        PrivatizeFinalClassMethodRector::class,
+        PrivatizeFinalClassPropertyRector::class,
+        ResponseHelperCallToJsonResponseRector::class,
+    ]);
+
+    $rectorConfig->cacheDirectory(__DIR__ . '/storage/rector');
+    $rectorConfig->cacheClass(FileCacheStorage::class);
+
+    $rectorConfig->sets([
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+        SetList::CODE_QUALITY,
+        SetList::PRIVATIZATION,
+        SetList::CODING_STYLE,
+        SetList::NAMING,
+        LevelSetList::UP_TO_PHP_84,
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_84);
+};
