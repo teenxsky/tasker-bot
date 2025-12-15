74376faa5bb0ee67b1aefc6144cf651391add551
teenxsky <kutorgin2002@gmail.com>
2025-12-16 01:05:26 +1000

    Remove unused migrations

--- config/queue.php
+++ config/queue.php
@@ -123,7 +123,7 @@ return [
     */
 
     'failed' => [
-        'driver'   => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
+        'driver'   => null,
         'database' => env('DB_CONNECTION', 'sqlite'),
         'table'    => 'failed_jobs',
     ],
