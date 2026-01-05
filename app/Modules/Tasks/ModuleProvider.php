282f4d5ed65266691a0f3a11b1116f04a19ab239
teenxsky <kutorgin2002@gmail.com>
2026-01-06 00:09:48 +1000

    Update tasks module provider

--- app/Modules/Tasks/ModuleProvider.php
+++ app/Modules/Tasks/ModuleProvider.php
@@ -4,22 +4,58 @@ declare(strict_types=1);
 
 namespace App\Modules\Tasks;
 
+use App\Modules\Tasks\Interfaces\Repositories\TaskExecutionResultRepositoryInterface;
+use App\Modules\Tasks\Interfaces\Repositories\TaskRepositoryInterface;
+use App\Modules\Tasks\Interfaces\Services\TaskExecutionResultServiceInterface;
+use App\Modules\Tasks\Interfaces\Services\TaskServiceInterface;
+use App\Modules\Tasks\Interfaces\UseCases\CreateTaskUseCaseInterface;
+use App\Modules\Tasks\Interfaces\UseCases\ProcessTaskUseCaseInterface;
+use App\Modules\Tasks\Repositories\TaskExecutionResultRepository;
+use App\Modules\Tasks\Repositories\TaskRepository;
+use App\Modules\Tasks\Services\TaskExecutionResultService;
+use App\Modules\Tasks\Services\TaskService;
+use App\Modules\Tasks\UseCases\CreateTaskUseCase;
+use App\Modules\Tasks\UseCases\ProcessTaskUseCase;
 use App\Shared\Abstracts\AbstractModuleProvider;
 
 final class ModuleProvider extends AbstractModuleProvider
 {
     protected function registerRepositories(): void
     {
-        // TODO: Implement registerRepositories() method.
+        $this->app->singleton(
+            TaskRepositoryInterface::class,
+            TaskRepository::class
+        );
+
+        $this->app->singleton(
+            TaskExecutionResultRepositoryInterface::class,
+            TaskExecutionResultRepository::class
+        );
     }
 
     protected function registerServices(): void
     {
-        // TODO: Implement registerServices() method.
+        $this->app->bind(
+            TaskServiceInterface::class,
+            TaskService::class
+        );
+
+        $this->app->bind(
+            TaskExecutionResultServiceInterface::class,
+            TaskExecutionResultService::class
+        );
     }
 
     protected function registerUseCases(): void
     {
-        // TODO: Implement registerUseCases() method.
+        $this->app->bind(
+            CreateTaskUseCaseInterface::class,
+            CreateTaskUseCase::class
+        );
+
+        $this->app->bind(
+            ProcessTaskUseCaseInterface::class,
+            ProcessTaskUseCase::class
+        );
     }
 }
